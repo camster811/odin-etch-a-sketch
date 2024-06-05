4d365bc849bf73193f6284eabe846cbe86f9c3ec
Camster811 <cls0150@auburn.edu>
2024-06-04 23:56:59 -0500

    add button to change grid size

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const container = document.getElementById("container");
-container.style.display = "flex";
-container.style.flexWrap = "wrap";
-container.style.width = `1632px`;
-container.style.height = "1632px";
+const edit = document.querySelector("button");
+let newGrid = 16;
 
 
 for (let i = 0; i < 256; i++) {
@@ -18,3 +16,29 @@ for (let i = 0; i < 256; i++) {
     });
 }
 
+edit.addEventListener('click', () => {
+    newGrid = prompt("Please enter squares per side of new grid")
+    while (newGrid > 100) {
+        newGrid = prompt("Please enter a valid value less than 100")
+    }
+    createNewGrid(newGrid);
+});
+
+function createNewGrid(gridSize) {
+    container.innerHTML = '';
+
+    for (let i = 0; i < (gridSize*gridSize); i++) {
+        let div = document.createElement('div');
+        let divSize = 1600/gridSize;
+
+        div.style.border = "solid";
+        div.style.borderWidth = "1px";
+        div.style.width = `${divSize}px`;
+        div.style.height = `${divSize}px`;
+        container.appendChild(div)
+    
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = "yellow";
+        });
+    }
+}
