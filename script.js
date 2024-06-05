7fea51966c3a5beb8bd6b7a68628a6f1c1bacbb5
Camster811 <cls0150@auburn.edu>
2024-06-04 23:21:28 -0500

    create 16x16 square grid

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById("container");
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.width = `1632px`;
+container.style.height = "1632px";
+
+
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement('div');
+    div.style.border = "solid";
+    div.style.borderWidth = "1px";
+    div.style.width = "100px";
+    div.style.height = "100px";
+    container.appendChild(div)
+}
